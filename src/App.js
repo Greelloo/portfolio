9d318c7944a24900605340a6261f8cb6224c42a9
greelloo <yusuffgrillo@gmail.com>
2022-03-22 11:08:04 +0100

    navbar completed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+};
+export default App;
